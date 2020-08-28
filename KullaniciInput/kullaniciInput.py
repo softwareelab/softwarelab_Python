4208e0d3cec577ddbbf899722f50f19ae0d56806
softwareelab <69670953+softwareelab@users.noreply.github.com>
2020-08-28 23:11:48 +0300

    Add files via upload

---
+++ KullaniciInput/kullaniciInput.py
@@ -0,0 +1,8 @@
+ad = input(str("Adınız:"))
+soyad = input(str("Soyadınız:"))
+yas = input("Yaşınız:")
+dogumyili = input("Doğum Yılınız:")
+
+print("-----Ekran Çıktısı-----", end="\n")
+print(ad, soyad, yas, sep="\n")
+print(dogumyili)
