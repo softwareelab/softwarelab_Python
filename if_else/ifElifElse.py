814f9cba902c4e03002b7d7f4025fa28506c4b9e
softwareelab <69670953+softwareelab@users.noreply.github.com>
2020-08-29 21:57:36 +0300

    Add files via upload

---
+++ if_else/ifElifElse.py
@@ -0,0 +1,28 @@
+girilenkelime = str(input("Bir kelime giriniz: "))
+
+if 'm' in girilenkelime:
+    print("m harfi var")
+if 'a' in girilenkelime:
+    print("a harfi var")
+if 'b' in girilenkelime:
+    print("b harfi var")
+if 'c' in girilenkelime:
+    print("c harfi var")
+if 'h' in girilenkelime:
+    print("h harfi var")
+if 'e' in girilenkelime:
+    print("e harfi var")
+if 'i' in girilenkelime:
+    print("i harfi var")
+if 'o' in girilenkelime:
+    print("o harfi var")
+if 'k' in girilenkelime:
+    print("k harfi var")
+if 'l' in girilenkelime:
+    print("l harfi var")
+if 't' in girilenkelime:
+    print("t harfi var")
+if 'r' in girilenkelime:
+    print("r harfi var")
+else:
+    print("m, a, b, c, h, e, i, o, k, l, t, r harfleri yok")
