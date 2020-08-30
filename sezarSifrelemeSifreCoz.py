9263f7ee143721b02dbedab5a8b32c503b259c56
softwareelab <69670953+softwareelab@users.noreply.github.com>
2020-08-30 22:53:53 +0300

    Add files via upload

---
+++ sezarSifrelemeSifreCoz.py
@@ -0,0 +1,10 @@
+alfabe = "abcçdefgğhıijklmnoöprsştuüvyzxwq abc"
+cumle = input("Cümle: ")
+yenicumle = ""
+for i in cumle:
+    print("Harf -->", i, "   ---Gerçek index -->", alfabe.index(i),
+          "   ---Şifrelenmiş index -->", alfabe.index(i) - 3, "   ---Şifreli karakter -->",
+          alfabe[alfabe.index(i) - 3])
+    indeks = alfabe.index(i)
+    yenicumle += alfabe[indeks - 3]
+print(yenicumle)
---
+++ sezarSifrelemeV2.py
@@ -0,0 +1,17 @@
+k_alfabe = "abcçdefgğhıijklmnoöprsştuüvyzxwq +-*\\/)({}&%$#^'!abc"
+b_alfabe = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZXWQ ABC"
+
+cumle = input("Cümle: ")
+yenicumle = ""
+for i in cumle:
+    if i in k_alfabe:
+        indeks = k_alfabe.index(i)
+        yenicumle += k_alfabe[indeks + 3]
+    elif i in b_alfabe:
+        indeks = b_alfabe.index(i)
+        yenicumle += b_alfabe[indeks + 3]
+    else:
+        print("bu karakter alfabe düzeninde mevcut değil!")
+        print("çevrilebilen kısım: ", yenicumle)
+        quit()
+print(yenicumle)
